883bfa6658ddef0eda1fb9642f97ed50162c049d
Dan <dan@danielgc.design>
2021-01-31 10:43:00 -0800

    commented out chenges

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,8 @@ import express, { Application, Request, Response, NextFunction } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import fetch from 'node-fetch';
+import { createProxyMiddleware, Filter, Options, RequestHandler, } from 'http-proxy-middleware';
+import { ClientRequest, IncomingMessage } from 'http';
 
 dotenv.config();
 
@@ -10,12 +12,37 @@ dotenv.config();
  * App Variables
  */
 if (!process.env.PORT) {
-	process.exit(1);
+  process.exit(1);
 }
 
 const app: Application = express();
 const PORT: number = parseInt(process.env.PORT as string, 10);
 const TWITTER_SEARCH_URL = `https://api.twitter.com/1.1/search/tweets.json`;
+
+
+// function onProxyReq(proxyReq: ClientRequest, req: Request, res: Response) {
+//   // add custom header to request
+//   // proxyReq.setHeader('x-added', 'foobar');
+//   console.log({ res })
+//   proxyReq.setHeader(`Authorization`, `Bearer ${process.env.TWITTER_BEARER_TOKEN}`);
+//   // or log the req
+// }
+
+// function onProxyRes(proxyRes: IncomingMessage, req: Request, res: Response) {
+//   proxyRes.headers[`Authorization`] = `Bearer ${process.env.TWITTER_BEARER_TOKEN}`); // add new header to response
+//   delete proxyRes.headers[`Authorization`]; // remove header from response
+// }
+
+// const twitterProxy = createProxyMiddleware({
+//   target: TWITTER_SEARCH_URL,
+//   changeOrigin: true,// for vhosted sites, changes host header to match to target's host
+//   logLevel: 'debug',
+//   onProxyReq,
+//   onProxyRes
+//   // pathRewrite: {
+//   //   [`^/1.1/search/tweets.json`]: '',
+//   // },
+// });
 
 /**
  *  App Configuration
@@ -25,32 +52,35 @@ app.use(morgan('common'));
 app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-	res.json({ status: 'success', message: `Welcome to my API service` });
+  res.json({ status: 'success', message: `Welcome to my API service` });
 });
 
+// app.use('/api', twitterProxy);
+
+
 app.get('/tweets', async (req: Request, res: Response) => {
-	const { query } = req;
-	const queryString = Object.keys(query)
-		.map((key) => key + '=' + query[key])
-		.join('&');
-	const response = await fetch(`${TWITTER_SEARCH_URL}?${queryString}`, {
-		headers: {
-			Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
-		},
-	});
-	const json = await response.json();
-	res.json(json);
+  const { query } = req;
+  const queryString = Object.keys(query)
+    .map((key) => key + '=' + query[key])
+    .join('&');
+  const response = await fetch(`${TWITTER_SEARCH_URL}?${queryString}`, {
+    headers: {
+      Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
+    },
+  });
+  const json = await response.json();
+  res.json(json);
 });
 
 function notFound(req: Request, res: Response, next: NextFunction) {
-	res.status(404);
-	const error = new Error('Not Found');
-	next(error);
+  res.status(404);
+  const error = new Error('Not Found');
+  next(error);
 }
 
 function errorHandler(error: Error, req: Request, res: Response, next: NextFunction) {
-	res.status(res.statusCode || 500);
-	res.json({ message: error.message });
+  res.status(res.statusCode || 500);
+  res.json({ message: error.message });
 }
 
 app.use(notFound);
@@ -61,5 +91,5 @@ app.use(errorHandler);
  */
 
 app.listen(PORT, () => {
-	console.log(`Listening on port ${PORT} 🚀 `);
+  console.log(`Listening on port ${PORT} 🚀 `);
 });
