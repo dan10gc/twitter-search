7bce2e74bd239cc5671426d26a40d53dc5d5c0b1
Dan <dan@danielgc.design>
2021-01-30 17:44:59 -0800

    added regex to jest config and github actions

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
     // [...]
     // Replace `ts-jest` with the preset you want to use
     // from the above list
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
     preset: 'ts-jest',
     setupFilesAfterEnv: ['<rootDir>/jest-setup.ts']
   }
